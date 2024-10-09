5db97d502c8132292398d94c6e410c327122982a
paul.blakeley1 <paul.blakeley1@engineering.digital.dwp.gov.uk>
2024-10-09 11:38:05 +0100

    Initial filter with GET and POST propagating values accordingly.

--- app/routes.js
+++ app/routes.js
@@ -7,39 +7,69 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 
-// Logging session data  
+// Logging session data
 // This code shows in the terminal what session data has been saved.
-router.use((req, res, next) => {    
-    const log = {  
-      method: req.method,  
-      url: req.originalUrl,  
-      data: req.session.data  
-    }  
-    console.log(JSON.stringify(log, null, 2))  
-   
-  next()  
-})  
-
-// This code shows in the terminal what page you are on and what the previous page was.
-router.use('/', (req, res, next) => {  
-    res.locals.currentURL = req.originalUrl; //current screen  
-    res.locals.prevURL = req.get('Referrer'); // previous screen
-  
-  console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
-  
-    next();  
-  });
-
-  // Routing for the example journey. 
-  router.post('/country-answer', function(request, response) {
-
-    var country = request.session.data['country']
-    if (country == "England"){
-        response.redirect("example/complete")
-    } else {
-        response.redirect("example/ineligible")
+router.use((req, res, next) => {
+    const log = {
+      method: req.method,
+      url: req.originalUrl,
+      data: req.session.data
     }
+    console.log(JSON.stringify(log, null, 2))
+
+  next()
 })
 
+// This code shows in the terminal what page you are on and what the previous page was.
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; //current screen
+    res.locals.prevURL = req.get('Referrer'); // previous screen
 
+  console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+
+    next();
+  });
+
+// Routing for the example journey.
+router.post('/country-answer', function(request, response) {
+
+  var country = request.session.data['country']
+  if (country == "England"){
+      response.redirect("example/complete")
+  } else {
+      response.redirect("example/ineligible")
+  }
+})
+
+router.get('/filter', function(request, response) {
+  view = {
+    count: 10,
+    types: getTypes()
+  }
+  response.render('/filter', view)
+})
+
+router.post('/filter', function(request, response) {
+  view = {
+    count: 10,
+    firstName: body.firstName,
+    types: getTypes(body.types),
+    status: body.status
+  }
+  response.render('/filter', view)
+})
+
+const types = ['Blue', 'Yellow', 'Red']
+const getTypes = (results=[]) => {
+  let items = []
+  types.forEach((item, index) => {
+    const checked= results.includes(item)
+    items.push({
+      value: item,
+      text: item,
+      checked: checked
+    })
+  })
+  return items
+}
   // Add your routes here
